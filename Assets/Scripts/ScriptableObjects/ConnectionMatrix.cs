11e8f2c762d1d2a7a91e514bb6c50c2a47f8bbb2
sinatb <sinatb.dev@gmail.com>
2024-10-11 23:30:27 +0330

    feat : made the skeleton more comprehensable
    ** changes **
    1- added a scale to skeleton generator
    2- Created a Connection matrix for body skeleton
    
    **TODO**
    1- Add visibility to skeleton generator calculations
    2- Add multi level connections to Connection Matrix

---
+++ Assets/Scripts/ScriptableObjects/ConnectionMatrix.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class ConnectionMatrix : ScriptableObject
+{
+    public List<int> matrix;    
+}
--- Assets/Scripts/player_skeleton_generator.cs
+++ Assets/Scripts/player_skeleton_generator.cs
@@ -6,6 +6,8 @@ public class player_skeleton_generator : MonoBehaviour
 {
     public GameObject joint;
     public GameObject initial_pos;
+    public ConnectionMatrix connectionMatrix;
+    public float scale;
     private List<GameObject> joints;
 
     private void Start()
@@ -22,11 +24,21 @@ public class player_skeleton_generator : MonoBehaviour
     {
         for(int i=0; i<32;i++)
         {
-            var point = new Vector3(pdl.points[i].x,
-                                    pdl.points[i].y, 
-                                    pdl.points[i].z
+            var point = new Vector3(pdl.points[i].x * scale,
+                                    -pdl.points[i].y * scale, 
+                                    pdl.points[i].z * scale
                                     );
             joints[i].transform.position = point;    
+        }
+        for  (int i=0; i<32;i++)
+        {
+            var lr = joints[i].GetComponent<LineRenderer>();
+            var index = connectionMatrix.matrix[i];
+            if (index < 0)
+                continue;
+            var destination = joints[index].transform.position;
+            lr.SetPosition(0, joints[i].transform.position);
+            lr.SetPosition(1, destination);
         }
     }
 }
