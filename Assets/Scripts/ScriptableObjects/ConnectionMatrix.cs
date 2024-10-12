d864019df57e5519fd7a526cb49a1297bf3eec28
sinatb <sinatb.dev@gmail.com>
2024-10-12 13:35:06 +0330

    feat : multilevel Connections added

--- Assets/Scripts/ScriptableObjects/ConnectionMatrix.cs
+++ Assets/Scripts/ScriptableObjects/ConnectionMatrix.cs
@@ -1,7 +1,14 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+[Serializable]
+public class MatrixEntry
+{
+    public int src;
+    public int dst;
+}
 [CreateAssetMenu]
 public class ConnectionMatrix : ScriptableObject
 {
-    public List<int> matrix;    
+    public List<MatrixEntry> matrix;    
 }
--- Assets/Scripts/player_skeleton_generator.cs
+++ Assets/Scripts/player_skeleton_generator.cs
@@ -6,23 +6,35 @@ public class player_skeleton_generator : MonoBehaviour
 {
     public GameObject joint;
     public GameObject initial_pos;
+    public GameObject lr_prefab;
     public ConnectionMatrix connectionMatrix;
     public float scale;
     private List<GameObject> joints;
-
+    private List<GameObject> lr_list;
     private void Start()
     {
         GameManager.update_data_event += update_data;
         joints = new List<GameObject>();
-        for (int i=0; i<32; i++)
+        lr_list = new List<GameObject>();
+        for (int i=0; i<33; i++)
         {
             var obj = Instantiate(joint, initial_pos.transform.position,Quaternion.identity,initial_pos.transform);
             joints.Add(obj);
         }
+        for (int i=0; i< connectionMatrix.matrix.Count; i++) 
+        {
+            var src_index = connectionMatrix.matrix[i].src;
+            var dst_index = connectionMatrix.matrix[i].dst;
+            var src_pos = joints[src_index].transform.position;
+            var dst_pos = joints[dst_index].transform.position;
+
+            var lr_obj = Instantiate(lr_prefab, src_pos, Quaternion.identity, joints[src_index].transform);
+            lr_list.Add(lr_obj);
+        }
     }
     public void update_data(PointDataList pdl) 
     {
-        for(int i=0; i<32;i++)
+        for(int i=0; i<pdl.points.Count;i++)
         {
             var point = new Vector3(pdl.points[i].x * scale,
                                     -pdl.points[i].y * scale, 
@@ -30,15 +42,16 @@ public class player_skeleton_generator : MonoBehaviour
                                     );
             joints[i].transform.position = point;    
         }
-        for  (int i=0; i<32;i++)
+        for (int i = 0; i < connectionMatrix.matrix.Count; i++)
         {
-            var lr = joints[i].GetComponent<LineRenderer>();
-            var index = connectionMatrix.matrix[i];
-            if (index < 0)
-                continue;
-            var destination = joints[index].transform.position;
-            lr.SetPosition(0, joints[i].transform.position);
-            lr.SetPosition(1, destination);
+            var lr = lr_list[i].GetComponent<LineRenderer>();
+            var src_index = connectionMatrix.matrix[i].src;
+            var dst_index = connectionMatrix.matrix[i].dst;
+            var src_pos = joints[src_index].transform.position;
+            var dst_pos = joints[dst_index].transform.position;
+
+            lr.SetPosition(0, src_pos);
+            lr.SetPosition(1, dst_pos);
         }
     }
 }
