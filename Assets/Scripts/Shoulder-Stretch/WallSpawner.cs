d6ab9864e1e841e3e366748232bcd8f7b587d095
Sina Taheri <79784688+sinatb@users.noreply.github.com>
2024-12-05 13:18:43 +0330

    feat : created an object pool for creating walls

--- Assets/Scripts/Shoulder-Stretch/WallSpawner.cs
+++ Assets/Scripts/Shoulder-Stretch/WallSpawner.cs
@@ -1,18 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WallSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject wallPrefab;
+    public float wallOffset;
+    public float spawnInterval;
+    public int poolSize;
+    private List<GameObject> _pool;
+    
+    private void Start()
     {
-        
+        _pool = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < poolSize; i++)
+        {
+            tmp = Instantiate(wallPrefab);
+            tmp.SetActive(false);
+            _pool.Add(tmp);
+        }
+        InvokeRepeating(nameof(SpawnWall),0.0f,spawnInterval);
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private GameObject GetPooledObject()
     {
-        
+        foreach (var g in _pool)
+        {
+            if (!g.activeInHierarchy)
+                return g;
+        }
+        return null;
     }
+    private void SpawnWall()
+    {
+        var side = Random.Range(-1, 2);
+        var spawnpos = new Vector3(transform.position.x - side *wallOffset,
+                                        1.5f,
+                                        transform.position.z);
+        GameObject wall = GetPooledObject();
+        wall.transform.position = spawnpos;
+        wall.SetActive(true);
+    }
+    
 }
