c49219ca841be63c0b4b7fa6ec21b2a7399ce4e1
Sina Taheri <79784688+sinatb@users.noreply.github.com>
2024-12-07 21:20:58 +0330

    feat : added vector utility functions

---
+++ Assets/Scripts/Util/VectorUtil.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+public class VectorUtil
+{
+        public static Vector3 MeanVector(List<Vector3> vectors)
+        {
+                var mean = Vector3.zero;
+                foreach (var v in vectors)
+                {
+                        mean += v;
+                }
+                return mean / vectors.Count;
+        } 
+}
