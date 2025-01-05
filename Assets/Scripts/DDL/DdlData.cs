a6f95995553c28230a89b7bce66bac0e34760538
Sina Taheri <79784688+sinatb@users.noreply.github.com>
2025-01-05 11:56:46 +0330

    feat : implemented ddl

--- Assets/Scripts/DDL/DdlData.cs
+++ Assets/Scripts/DDL/DdlData.cs
@@ -4,14 +4,14 @@ namespace DDL
     [CreateAssetMenu(menuName = "DDL/Data", fileName = "DDL Data")]
     public class DdlData : ScriptableObject
     {
-        public int   Min;
-        public int   Max;
-        public float BaseSpeed;
-        public float BaseSpawnInterval;
+        public int   min;
+        public int   max;
+        public float baseSpeed;
+        public float baseSpawnInterval;
 
         public bool InRange(int level)
         {
-            return level >= Min && level <= Max;
+            return level >= min && level <= max;
         }
     }   
 }
--- Assets/Scripts/Overhead-Stretch/ObstacleSpawner.cs
+++ Assets/Scripts/Overhead-Stretch/ObstacleSpawner.cs
@@ -1,120 +1,131 @@
 using System.Collections.Generic;
+using DDL;
 using UnityEngine;
 
-public class ObstacleSpawner : MonoBehaviour
+namespace Overhead_Stretch
 {
-    public GameObject       obstaclePrefab;
-    public List<GameObject> spawnPosition;
-    public int              poolSize;
-    public float            increaseSpeed;
-    public float            maximumIncrease;
-    public float            increaseTime;
-    public float            spawnInterval;
-    public float            baseSpeed;
-    
-    private List<GameObject> _pool;
-    private float            _timer;
-    private bool             _isRunning = true;
-    private float            _currentSpeed;
-    private float            _currentSpawnInterval;
-    
-    private void Start()
+    public class ObstacleSpawner : MonoBehaviour
     {
-        _pool = new List<GameObject>();
-        _currentSpeed = baseSpeed;
-        _currentSpawnInterval = spawnInterval;
-        if (GameManager.Instance.Player != null)
+        public GameObject       obstaclePrefab;
+        public List<GameObject> spawnPosition;
+        public int              poolSize;
+        public float            increaseSpeed;
+        public float            maximumIncrease;
+        public float            increaseTime;
+        public float            spawnInterval;
+        public float            baseSpeed;
+        public List<DdlData>    dynamicDifficultyData;
+        
+        private List<GameObject> _pool;
+        private float            _timer;
+        private bool             _isRunning = true;
+        private float            _currentSpeed;
+        private float            _currentSpawnInterval;
+        
+        private void Start()
         {
+            _pool = new List<GameObject>();
+            _currentSpeed = baseSpeed;
+            _currentSpawnInterval = spawnInterval;
+            if (GameManager.Instance.Player != null)
+            {
+                foreach (var d in dynamicDifficultyData)
+                {
+                    if (d.InRange(GameManager.Instance.Player.HighScore))
+                    {
+                        _currentSpeed = d.baseSpeed;
+                        _currentSpawnInterval = d.baseSpawnInterval;
+                    }
+                }
+            }
+            
+            GameManager.GameOverEvent += GameOverFunction;
+            GameManager.RestartEvent += RestartFunction;
+            GameObject tmp;
+            for (var i = 0; i < poolSize; i++)
+            {
+                tmp = Instantiate(obstaclePrefab);
+                tmp.SetActive(false);
+                _pool.Add(tmp);
+            }
+            InvokeRepeating(nameof(UpdateSpeed),0.0f,increaseTime);
+        }
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= _currentSpawnInterval && _isRunning)
+            {
+                SpawnObstacle();    
+                _timer = 0.0f;
+            }
+        }
+        private GameObject GetPooledObject()
+        {
+            foreach (var g in _pool)
+            {
+                if (!g.activeInHierarchy)
+                    return g;
+            }
+            return null;
+        }
+        private void SpawnObstacle()
+        {
+            var side = Random.Range(0, 2);
+            
+            var horizontalCoef = 0.6f;
+            if (side == 0)
+                horizontalCoef = 1.0f;
+            else if (side == 1)
+                horizontalCoef = 1.5f;
+            
+                    
+            var w1 = GetPooledObject();
+            var spawnPosition1 = spawnPosition[side].transform.position;
+            spawnPosition1.x += horizontalCoef;
+            w1.transform.position = spawnPosition1;
+            w1.GetComponent<ObstacleBehaviour>().SetSpeed(_currentSpeed);
+
+            w1.SetActive(true);
+            
+            var w2 = GetPooledObject();
+            var spawnPosition2 = spawnPosition[side].transform.position;
+            spawnPosition2.x -= horizontalCoef;
+            w2.transform.position = spawnPosition2;
+            w2.GetComponent<ObstacleBehaviour>().SetSpeed(_currentSpeed);
+            
+            w2.SetActive(true);
             
         }
-        
-        GameManager.GameOverEvent += GameOverFunction;
-        GameManager.RestartEvent += RestartFunction;
-        GameObject tmp;
-        for (var i = 0; i < poolSize; i++)
+        private void UpdateSpeed()
         {
-            tmp = Instantiate(obstaclePrefab);
-            tmp.SetActive(false);
-            _pool.Add(tmp);
+            if (_currentSpeed + increaseSpeed > maximumIncrease)
+            {
+                CancelInvoke(nameof(UpdateSpeed));
+                return;
+            }
+            _currentSpeed += increaseSpeed;
+            _currentSpawnInterval -= 0.1f;
+            foreach (var g in _pool)
+            {
+                if (g.activeInHierarchy)
+                    g.GetComponent<ObstacleBehaviour>().SetSpeed(_currentSpeed);
+            }
         }
-        InvokeRepeating(nameof(UpdateSpeed),0.0f,increaseTime);
-    }
-    
-    private void Update()
-    {
-        _timer += Time.deltaTime;
-        if (_timer >= _currentSpawnInterval && _isRunning)
+        private void RestartFunction()
         {
-            SpawnObstacle();    
+            _currentSpeed = baseSpeed;
+            _currentSpawnInterval = spawnInterval;
+            _isRunning = true;
             _timer = 0.0f;
         }
-    }
-    private GameObject GetPooledObject()
-    {
-        foreach (var g in _pool)
+        private void GameOverFunction()
         {
-            if (!g.activeInHierarchy)
-                return g;
-        }
-        return null;
-    }
-    
-    private void SpawnObstacle()
-    {
-        var side = Random.Range(0, 2);
-        
-        var horizontalCoef = 0.6f;
-        if (side == 0)
-            horizontalCoef = 1.0f;
-        else if (side == 1)
-            horizontalCoef = 1.5f;
-        
-                
-        var w1 = GetPooledObject();
-        var spawnPosition1 = spawnPosition[side].transform.position;
-        spawnPosition1.x += horizontalCoef;
-        w1.transform.position = spawnPosition1;
-        w1.GetComponent<ObstacleBehaviour>().SetSpeed(_currentSpeed);
-
-        w1.SetActive(true);
-        
-        var w2 = GetPooledObject();
-        var spawnPosition2 = spawnPosition[side].transform.position;
-        spawnPosition2.x -= horizontalCoef;
-        w2.transform.position = spawnPosition2;
-        w2.GetComponent<ObstacleBehaviour>().SetSpeed(_currentSpeed);
-        
-        w2.SetActive(true);
-        
-    }
-    private void UpdateSpeed()
-    {
-        if (_currentSpeed + increaseSpeed > maximumIncrease)
-        {
-            CancelInvoke(nameof(UpdateSpeed));
-            return;
-        }
-        _currentSpeed += increaseSpeed;
-        _currentSpawnInterval -= 0.1f;
-        foreach (var g in _pool)
-        {
-            if (g.activeInHierarchy)
-                g.GetComponent<ObstacleBehaviour>().SetSpeed(_currentSpeed);
-        }
-    }
-    private void RestartFunction()
-    {
-        _currentSpeed = baseSpeed;
-        _currentSpawnInterval = spawnInterval;
-        _isRunning = true;
-        _timer = 0.0f;
-    }
-    private void GameOverFunction()
-    {
-        _isRunning = false;
-        foreach (var go in _pool)
-        {
-            go.SetActive(false);
+            _isRunning = false;
+            foreach (var go in _pool)
+            {
+                go.SetActive(false);
+            }
         }
     }
 }
+
