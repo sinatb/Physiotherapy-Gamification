5a1ef39f0ad770ce95a1861a9fcf27e90ae66a4c
Sina Taheri <79784688+sinatb@users.noreply.github.com>
2025-01-06 18:09:44 +0330

    feat : created object pool utility (finally)

---
+++ Assets/Scripts/Util/ObjectPool.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Util
+{
+    public class ObjectPool:MonoBehaviour
+    {
+        public GameObject prefab;
+        public int poolSize;
+        private List<GameObject> _pool = new List<GameObject>();
+
+        private void Awake()
+        {
+            GameObject tmp;
+            for (var i = 0; i < poolSize; i++)
+            {
+                tmp = Instantiate(prefab);
+                tmp.SetActive(false);
+                _pool.Add(tmp);
+            }
+        }
+        public GameObject GetPooledObject()
+        {
+            foreach (var g in _pool)
+            {
+                if (!g.activeInHierarchy)
+                    return g;
+            }
+            return null;
+        }
+
+        public List<GameObject> GetActiveObjects()
+        {
+            var result = new List<GameObject>();
+            foreach (var g in _pool)
+            {
+                if (g.activeInHierarchy)
+                    result.Add(g);
+            }
+            return result;
+        }
+
+        public void DeactivateObjects()
+        {
+            foreach (var g in _pool)
+            {
+                g.SetActive(false);
+            }
+        }
+    }
+}
