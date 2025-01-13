d825c365fcb55f55f25a7bffb2241ac5756d449d
Sina Taheri <79784688+sinatb@users.noreply.github.com>
2025-01-13 22:21:06 +0330

    feat : updated overhead stretch looks

---
+++ Assets/Scripts/Graphics/SpaceManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+namespace Graphics
+{
+    public class SpaceManager : MonoBehaviour
+    {
+        public float          skyBoxRotationSpeed;
+        public List<Material> skyBoxes;
+
+        private void Start()
+        {
+            RenderSettings.skybox = skyBoxes[Random.Range(0, skyBoxes.Count)];
+        }
+
+        private void Update()
+        {
+            RenderSettings.skybox.SetFloat("_Rotation",Time.time*skyBoxRotationSpeed);
+        }
+    }
+}
